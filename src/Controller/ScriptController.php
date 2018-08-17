e3490602895c3fe307bcb89e9d99ddfd72478f0c
xrkzor <nagromnoel@gmail.com>
2018-08-17 14:19:12 +0200

    Amelioration du ScriptController avec l'ajout des exports XML

--- src/Controller/ScriptController.php
+++ src/Controller/ScriptController.php
@@ -31,7 +31,7 @@ class ScriptController extends Controller
             $options .= "<option value='{$field->getFieldName()}'>{$field->getTitle()}</option>";
         }
         return $this->render('script/script.html.twig', [
-            'options' => $options, 'fields' => $fields 
+            'options' => $options, 'fields' => $fields, 'type' => $type
         ]);
     }
 
@@ -39,13 +39,21 @@ class ScriptController extends Controller
      * @Route("/script/traitement", name="traitement_script")
      */
     public function traitement(Request $request, FieldRepository $repo) {
+
+      
+        $type =  $request->request->get('type');
+        
+        if($type === "Web"){ 
+       
         $fields = $request->request->get('champs');
         
         $template = file_get_contents('../src/Controller/template.html');
 
         $sql = []; 
 
-        $textareas = []; 
+        $urlField = [];
+
+        $textareas = [];
 
         $alphabet = "abcdefghijklmnopq";
 
@@ -59,12 +67,15 @@ class ScriptController extends Controller
             if(!$infos){
                 $query = $fieldName;
                 $render = "~?x~";
+                $url = $fieldName;
             } else {
                 $query = $infos->getQuery();
                 $render = $infos->getRender();
             }
 
             $sql[] = $query;
+
+            $urlField[] = $url;
             
             $render = str_replace("x", $alphabet[$numero], $render);
 
@@ -82,10 +93,13 @@ class ScriptController extends Controller
             $numero++;
         }
 
+        $urlField = join($urlField);
+
         $sql = implode(", ", $sql);
 
         $textareas = join("\n", $textareas);
 
+        $template = str_replace('{{URL}}', $urlField, $template);
 
         $template = str_replace('{{SQL}}', $sql, $template);
 
@@ -105,6 +119,82 @@ class ScriptController extends Controller
 
         readfile("./titres_web.html");
         exit;
+    
+    } else {
+
+        $utilisateur = $this->getUser();
+        $radio = $utilisateur->getRadio();
+
+        $fields = $request->request->get('champs');
+        
+        $template = file_get_contents('../src/Controller/templatexml.html');
+
+        $sql = []; 
+        $textareas = []; 
+        $radios = [];
+        $radios[] = $radio;
+
+        $alphabet = "abcdefghijklmnopq";
+
+        $numero = 0;
+
+        foreach($fields as $fieldName) {
+            if(empty($fieldName)) continue;
+
+            $infos = $repo->findOneByFieldName($fieldName);
+
+            if(!$infos){
+                $query = $fieldName;
+                $render = "~?x~";
+            } else {
+                $query = $infos->getQuery();
+                $render = $infos->getRender();
+            }
+
+            $sql[] = $query;
+            $urlField[] = $url;
+            
+            $render = str_replace("x", $alphabet[$numero], $render);
+
+            if($infos && $infos->getPlaceholders() == 2) {
+                $numero++; 
+                $render = str_replace("y", $alphabet[$numero], $render);
+            } 
+
+            $textarea = "<$fieldName>$render</$fieldName>";
+         
+            $textareas[] = $textarea;
+
+            $numero++;
+        }
+
+        $sql = implode(", ", $sql);
+
+        $textareas = join("\n", $textareas);
+
+        $radios = join("\n\t", $radios);
+
+        $template = str_replace('{{RADIO}}', $radios, $template);
+
+        $template = str_replace('{{SQL}}', $sql, $template);
+
+        $template = str_replace('{{TEXTAREAS}}', $textareas, $template);
+
+
+        file_put_contents('./export_xml.html', $template);
+        
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename("./export_xml.html").'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize("./export_xml.html"));
+        flush();
+
+        readfile("./export_xml.html");
+        exit;
+        }
     }
 
 }
